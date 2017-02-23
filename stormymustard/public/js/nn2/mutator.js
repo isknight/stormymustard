d56b9a9c14f4b935f315a5edd99114d0efaf8d2c
jason <jason@noisybox.net>
2017-02-22 20:45:01 -0800

    var -> let

--- stormymustard/public/js/nn2/mutator.js
+++ stormymustard/public/js/nn2/mutator.js
@@ -1,11 +1,11 @@
 
 function mutateNetwork(network) {
 
-        var mutationCount = 2; //this._randomInteger(NNUtil.getAllNeurons(network).length,0);
+        let mutationCount = 2; //this._randomInteger(NNUtil.getAllNeurons(network).length,0);
 
-        for (var i=0; i < mutationCount; i++) {
+        for (let i=0; i < mutationCount; i++) {
 
-            var whichMutation = this._randomInteger(100, 0);
+            let whichMutation = this._randomInteger(100, 0);
 
             if (whichMutation > 5) {
                 //TODO pruning algo, might not be useful for this project
@@ -23,7 +23,6 @@ function mutateNetwork(network) {
             if (whichMutation > 40 && whichMutation < 100) {
                 this._randomlyMutateSynapseWeight(network);
             }
-
         }
         //console.log('neuron count=' + network.neurons.length);
         Network.resetMaxOutputs(network);
@@ -36,10 +35,10 @@ function _randomlyAddNeuron(network) {
 
     network.generation += 1;
 
-    var neuron = new Neuron('h', network.generation);
+    let neuron = new Neuron('h', network.generation);
 
-    var neuronsA = [];
-    var neuronsB = [];
+    let neuronsA = [];
+    let neuronsB = [];
 
     // assuming neurons A->B
     //first get a list of potential Bs
@@ -48,24 +47,24 @@ function _randomlyAddNeuron(network) {
 
 
     //pick a random B
-    var index = this._randomInteger(neuronsB.length, 0);
-    var neuronB = neuronsB[index];
+    let index = this._randomInteger(neuronsB.length, 0);
+    let neuronB = neuronsB[index];
 
     //now find a list of potential As through all of B's connections
-    for (var i in neuronB.connections) {
-        //var n = NNUtil.getNeuronById(neuronB.connections[i].neuronId, network);
-        var n = network.neurons[neuronB.connections[i].neuronId];
+    for (let i in neuronB.connections) {
+        //let n = NNUtil.getNeuronById(neuronB.connections[i].neuronId, network);
+        let n = network.neurons[neuronB.connections[i].neuronId];
         neuronsA.push(n);
     }
 
     index = this._randomInteger(neuronsA.length, 0);
 
-    var neuronA = neuronsA[index];
-    var connectionA = null;
+    let neuronA = neuronsA[index];
+    let connectionA = null;
 
     //Find the connection to hijack
-    for (var i in neuronB.connections) {
-        var connection = neuronB.connections[i];
+    for (let i in neuronB.connections) {
+        let connection = neuronB.connections[i];
         if(connection.neuronId == neuronA.id) {
             connectionA = connection;
         }
@@ -86,23 +85,23 @@ function _randomlyAddNeuron(network) {
 }
 
 function _randomlyAddSynapseWithRandomWeight(network) {
-    var neurons = [];
-    var connections = [];
+    let neurons = [];
+    let connections = [];
 
     //First get all of the hidden and output neurons, to randomly select
     //a neuron eligible for a new input. Input neurons cannot take part.
     neurons.push.apply(neurons, network.hiddenNeurons);
     neurons.push.apply(neurons, network.outputNeurons);
 
-    var randomNeuronIndex = this._randomInteger(neurons.length, 0);
-    var randomNeuron = neurons[randomNeuronIndex];
+    let randomNeuronIndex = this._randomInteger(neurons.length, 0);
+    let randomNeuron = neurons[randomNeuronIndex];
 
     //a list of neurons this random neuron is already connected to. We don't want a double connection.
-    var alreadyConnections = [];
-    for (var i in randomNeuron.connections) {
-        var connection = randomNeuron.connections[i];
-        //var n = NNUtil.getNeuronById(connection.neuronId, network);
-        var n = network.neurons[connection.neuronId];
+    let alreadyConnections = [];
+    for (let i in randomNeuron.connections) {
+        let connection = randomNeuron.connections[i];
+        //let n = NNUtil.getNeuronById(connection.neuronId, network);
+        let n = network.neurons[connection.neuronId];
         alreadyConnections.push(n);
     }
 
@@ -111,11 +110,11 @@ function _randomlyAddSynapseWithRandomWeight(network) {
     neurons.push.apply(neurons, network.inputNeurons);
     neurons.push.apply(neurons, network.hiddenNeurons);
 
-    var newPotentialConnections = neurons.filter(function(x) { return alreadyConnections.indexOf(x) < 0 })
+    let newPotentialConnections = neurons.filter(function(x) { return alreadyConnections.indexOf(x) < 0 })
 
     if (newPotentialConnections.length > 0) {
         randomNeuronIndex = this._randomInteger(newPotentialConnections.length, 0);
-        var randomConnectingNeuron = newPotentialConnections[randomNeuronIndex];
+        let randomConnectingNeuron = newPotentialConnections[randomNeuronIndex];
         Network.connect(this._randomWeight(1, 0), randomNeuron, randomConnectingNeuron);
     }
 }
@@ -125,8 +124,8 @@ function _randomlyAddSynapseWithRandomWeight(network) {
  */
 function _randomlyMutateSynapseWeight(network) {
 
-    var neurons = [];
-    var connections = [];
+    let neurons = [];
+    let connections = [];
 
     neurons.push.apply(neurons, network.inputNeurons);
     neurons.push.apply(neurons, network.hiddenNeurons);
@@ -135,14 +134,14 @@ function _randomlyMutateSynapseWeight(network) {
     //TODO add bias neuron back in
    // neurons.push(network.bias);
 
-    for (var i in neurons) {
-        var neuron = neurons[i];
+    for (let i in neurons) {
+        let neuron = neurons[i];
         connections.push.apply(connections, neuron.connections);
     }
 
-    var r = this._randomInteger(connections.length, 0);
+    let r = this._randomInteger(connections.length, 0);
 
-    var randomConnection = connections[r];
+    let randomConnection = connections[r];
 
     randomConnection.weight += this._randomWeight(.4,0) - .2;
 }
