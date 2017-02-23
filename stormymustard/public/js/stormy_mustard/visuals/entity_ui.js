177361ac2bbb3b433f2544be0beb04fdcd41456f
jason <jason@noisybox.net>
2017-02-22 20:38:49 -0800

    entity ui to class

--- stormymustard/public/js/stormy_mustard/visuals/entity_ui.js
+++ stormymustard/public/js/stormy_mustard/visuals/entity_ui.js
@@ -1,4 +1,8 @@
-function EntityUI(x, y, entity) {
+
+class EntityUI {
+
+  constructor(x, y, entity) {
+    this.i = 0;
     this.x = x;
     this.y = y;
     this.width = 100;
@@ -6,123 +10,114 @@ function EntityUI(x, y, entity) {
     this.entity = entity;
     this.playing = false;
     this.selected = false;
-};
+  }
 
-EntityUI.prototype = (function () {
-    return {
-        run: function (tick) {
-            if (this.playing) {
-                if (this.i < StormyMustardConfig.MAX_VOICE_DURATION) {
-                    this.entity.run(this.i);
-                } else {
-                    this.i = 0;
-                }
-                this.i++;
-            }
-        },
+  run(tick) {
+      if (this.playing) {
+          if (this.i < StormyMustardConfig.MAX_VOICE_DURATION) {
+              this.entity.run(this.i);
+          } else {
+              this.i = 0;
+          }
+          this.i++;
+      }
+  }
 
-        render: function (tick, graphics) {
+  render(tick, graphics) {
 
-            graphics.lineStyle(1, 0x00ff00);
-            graphics.drawRect(this.x, this.y, this.width, this.height);
+      graphics.lineStyle(1, 0x00ff00);
+      graphics.drawRect(this.x, this.y, this.width, this.height);
 
+      if (this.selected) {
+          graphics.lineStyle(3, 0xffffff);
+          graphics.drawRect(this.x + 2, this.y + 2, this.width - 2, this.height - 2);
+      }
 
-            if (this.selected) {
-                graphics.lineStyle(3, 0xffffff);
-                graphics.drawRect(this.x + 2, this.y + 2, this.width - 2, this.height - 2);
-            }
+      this._renderNN(graphics);
 
-            this._renderNN(graphics);
+      if (this.playing) {
+          this._renderPlaying2(tick, graphics);
+      }
+  }
 
-            if (this.playing) {
-                this._renderPlaying2(tick, graphics);
-            }
-        },
+  _renderPlaying2(tick, graphics) {
 
-        _renderPlaying2: function(tick, graphics) {
+      let padding = 5;
+      let points = [];
+      points.push({x:this.x - padding, y:this.y - padding});
+      points.push({x:this.x + padding*2 + this.width, y:this.y - padding});
+      points.push({x:this.x + padding*2 + this.width, y:this.y + padding*2 + this.height});
+      points.push({x:this.x - padding, y:this.y + padding*2 + this.height});
+      points.push({x:this.x - padding, y:this.y - padding});
 
-            var padding = 5;
-            var points = [];
-            points.push({x:this.x - padding, y:this.y - padding});
-            points.push({x:this.x + padding*2 + this.width, y:this.y - padding});
-            points.push({x:this.x + padding*2 + this.width, y:this.y + padding*2 + this.height});
-            points.push({x:this.x - padding, y:this.y + padding*2 + this.height});
-            points.push({x:this.x - padding, y:this.y - padding});
+      this._renderOutline(points, tick, graphics);
+  }
 
-            this._renderOutline(points, tick, graphics);
+  _renderOutline(points, tick, graphics) {
+      let lastPoint = null;
 
-        },
+      let i = points.length;
+      while (i--) {
+          if (lastPoint) {
+              Electricity.fireWeapon(lastPoint, points[i], graphics, 0xffffff, 2, 1);
+          }
 
-        _renderOutline: function(points, tick, graphics) {
-            var lastPoint = null;
+          lastPoint = points[i];
+      }
+  }
 
-            var i = points.length;
-            while (i--) {
-                if (lastPoint) {
-                    Electricity.fireWeapon(lastPoint, points[i], graphics, 0xffffff, 2, 1);
-                }
+  _renderPlaying(tick, graphics) {
+      let startingPaddingSize = 2;
+      let endingPaddingSize = 20;
+      let alpha = (tick % 10) / 10;
+      let alphaStep = 1 / (endingPaddingSize / startingPaddingSize);
 
-                lastPoint = points[i];
-            }
-        },
+      while (startingPaddingSize <= endingPaddingSize) {
+          graphics.lineStyle(1, 0xffffff, alpha);
+          graphics.drawRect(this.x - startingPaddingSize, this.y - startingPaddingSize, this.width + startingPaddingSize*2, this.height + startingPaddingSize*2);
 
-        _renderPlaying: function(tick, graphics) {
-            var startingPaddingSize = 2;
-            var endingPaddingSize = 20;
-            var alpha = (tick % 10) / 10;
-            var alphaStep = 1 / (endingPaddingSize / startingPaddingSize);
+          startingPaddingSize++;
+          alpha -= alphaStep;
+          if (alpha < 0) {
+              alpha = 1;
+          }
+      }
+  }
 
-            while (startingPaddingSize <= endingPaddingSize) {
-                graphics.lineStyle(1, 0xffffff, alpha);
-                graphics.drawRect(this.x - startingPaddingSize, this.y - startingPaddingSize, this.width + startingPaddingSize*2, this.height + startingPaddingSize*2);
+  _renderNN(graphics) {
+      let rows = this.entity.voices.length;
+      let rowHeight = (this.height - 10) / rows;
 
-                startingPaddingSize++;
-                alpha -= alphaStep;
-                if (alpha < 0) {
-                    alpha = 1;
-                }
-            }
-        },
+      for(let i = 10; i < this.width - 10; i+=2) {
+          let inputHeight = (i / this.width) * 2 - 1;
+          for (let k in this.entity.voices) {
+              let output = Network.fire(this.entity.voices[k].nn, [inputHeight]);
+              let rgb = [];
+              rgb[0] = ((output[0] + 1) / 2) * 255;
+              rgb[2] = ((output[1] + 1) / 2) * 255;
+              rgb[1] = (rgb[0] + rgb[2]) / 2;
 
-        _renderNN: function (graphics) {
-            var rows = this.entity.voices.length;
-            var rowHeight = (this.height - 10) / rows;
+              let c = this._rgb2hex(rgb);
+              graphics.lineStyle(1, c);
+              //graphics.beginFill(c, 1);
+              graphics.drawRect(this.x + i, this.y + rowHeight * k + 4, 2, rowHeight);
+          }
+      }
+  }
 
-            for(var i = 10; i < this.width - 10; i+=2) {
-                var inputHeight = (i / this.width) * 2 - 1;
-                for (var k in this.entity.voices) {
-                    var output = Network.fire(this.entity.voices[k].nn, [inputHeight]);
-                    var rgb = [];
-                    rgb[0] = ((output[0] + 1) / 2) * 255;
-                    rgb[2] = ((output[1] + 1) / 2) * 255;
-                    rgb[1] = (rgb[0] + rgb[2]) / 2;
+  _rgb2hex(rgb) {
+      return ((rgb[0]*255 << 16) + (rgb[1]*255 << 8) + rgb[2]*255);
+  }
 
-                    var c = this._rgb2hex(rgb);
-                    graphics.lineStyle(1, c);
-                    //graphics.beginFill(c, 1);
+  play() {
+      this.entity.start();
+      this.i = 0;
+      this.playing = true;
+  }
 
-                    graphics.drawRect(this.x + i, this.y + rowHeight * k + 4, 2, rowHeight);
-                }
-
-            }
-        },
-
-        _rgb2hex: function (rgb) {
-            return ((rgb[0]*255 << 16) + (rgb[1]*255 << 8) + rgb[2]*255);
-        },
-
-
-        play: function () {
-            this.entity.start();
-            this.i = 0;
-            this.playing = true;
-        },
-        stop: function () {
-            this.entity.stop();
-            this.i = 0;
-            this.playing = false;
-        }
-
-    };
-
-})();
+  stop() {
+      this.entity.stop();
+      this.i = 0;
+      this.playing = false;
+  }
+}
